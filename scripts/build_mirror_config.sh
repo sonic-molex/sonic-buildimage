f9bb99a93fd551f2faf3c368eb264dafd18586d1
jimmy <jimmy.jin@molex.com>
2025-02-14 17:13:19 -0800

    Support OTN
    change to github path for submodules
    
    Modify config to disable some features not used on ot platform
    
    Fix EOL mirror issues for Buster and Bullseye by updating MIRROR_URLS
    
    The Bullseye release is now end-of-life, and the `bullseye-backports` entry
    is no longer available on the standard Debian mirrors, causing Docker image
    builds to fail.
    
    Due to the way `files/apt/sources.list.j2` is templated, the backports URL is
    generated using the shared `MIRROR_URLS` variable, which also affects the
    main and updates entries. Rather than restructuring the template to special-
    case `bullseye-backports`, this commit updates `DEFAULT_MIRROR_URLS` entirely
    for Bullseye to point to archive-capable mirrors that support all required
    repositories. In the case of Buster, `DEFAULT_MIRROR_SECURITY_URLS` was also
    updated to resolve similar build failures.
    
    - For Bullseye: uses `archive.debian.org` for main, updates, and backports,
    and `debian-archive.trafficmanager.net` specifically for security.
    - For Buster: uses `archive.debian.org` for main, updates backports and security.
    
    This approach keeps the templating logic simple and ensures stable builds
    for EOL releases using fully supported mirror endpoints.
    
    Fixes #23430 and #23336
    
    Signed-off-by: Alberto Martin <alberto.martin@htecgroup.com>
    
    Support otn-kvm platform for OTN devices
    
    Signed-off-by: oplklum <lu.mao@molex.com>
    
    merge event and alarm management related to  official #22617
    
    extend event function for platform event function
    
    changed libsai version to 1.1.0
    
    use hal server version 1.1.0
    
    supervisord does not spawn hal and HalApiServer anymore
    
    Fix submodule pointer for sonic-platform-daemons
    
    Update submodule sonic-swss-common to 202411_otn
    
    Update submodule sonic-sairedis to 202411_otn
    
    Update submodule sonic-swss 202411_otn
    
    Update submodule sonic-utilities 202411_otn

--- scripts/build_mirror_config.sh
+++ scripts/build_mirror_config.sh
@@ -27,9 +27,9 @@ fi
 
 if [ "$DISTRIBUTION" == "buster" ]; then
     DEFAULT_MIRROR_URLS=http://archive.debian.org/debian/
-    DEFAULT_MIRROR_SECURITY_URLS=http://archive.debian.org/debian-security/
+    DEFAULT_MIRROR_SECURITY_URLS=http://archive.debian.org/debian-security
 fi
-
+ 
 if [ "$DISTRIBUTION" == "bullseye" ]; then
     DEFAULT_MIRROR_URLS=http://archive.debian.org/debian/
 fi
