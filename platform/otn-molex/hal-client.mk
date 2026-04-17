3e33a02d510d29deabbcdc8db7c9603714779e42
pkable <183417605@qq.com>
2026-04-17 01:44:19 -0700

    update release url

--- platform/otn-molex/hal-client.mk
+++ platform/otn-molex/hal-client.mk
@@ -1,5 +1,5 @@
 HALCLIENT_VERSION = 1.1.0
 MOLEX_HALCLIENT_DEB = libhalplatformclient-mlx-$(HALCLIENT_VERSION)-amd64.deb
-$(MOLEX_HALCLIENT_DEB)_URL = "https://raw.githubusercontent.com/sonic-molex/sonic-libmlx/refs/heads/ocs/$(MOLEX_HALCLIENT_DEB)"
+$(MOLEX_HALCLIENT_DEB)_URL = "$(OTN_LIBS_RELEASE_URL)/$(MOLEX_HALCLIENT_DEB)"
 
 SONIC_ONLINE_DEBS += $(MOLEX_HALCLIENT_DEB)
